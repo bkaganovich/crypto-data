ab058c0d4546d1497a3edfb7c9746c8e8db6d242
bkaganovich <bennykaganovich@gmail.com>
2021-06-09 17:54:59 -0400

    updated variable name

--- history.py
+++ history.py
@@ -6,10 +6,10 @@ import sys
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
-coin = sys.argv[1] #take coin id as first arg
-to = sys.argv[2] #value in usd/btc as second arg
-day = sys.argv[3] #take date (dd-mm-yyyy) as third arg
+coin = sys.argv[1] #coin id as first arg
+vs_currency = sys.argv[2] #vs_currency pair as second arg
+day = sys.argv[3] #date (dd-mm-yyyy) as third arg
 
 history = cg.get_coin_history_by_id(id=coin,date=day,localization='false') #retrieve data from cg
-print((history['market_data']['current_price'][to])) #get converted value for specified date
+print((history['market_data']['current_price'][vs_currency])) #get converted value for specified date
 
